375cc291aea8b10eb8b4c561cf9997c7e1b0cb70
ezasewe <42439746+ezasewe@users.noreply.github.com>
2024-07-03 10:57:48 +0300

    [skip ci]

--- ascii-script.sh
+++ ascii-script.sh
@@ -4,3 +4,4 @@ cowsay -f dragon "Run for cover, Monster Coming ...RAWR" >> dragon.txt
 find ~ -name dragon -type f -print
 cat dragon.txt
 # testing workflow event activities and filters
+#skipping workflow runs 

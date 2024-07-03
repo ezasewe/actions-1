103a26a043d4be0855b9a5b7ddbde68c282d49e1
ezasewe <42439746+ezasewe@users.noreply.github.com>
2024-07-03 10:32:35 +0300

    testing event filters

--- ascii-script.sh
+++ ascii-script.sh
@@ -3,3 +3,4 @@ sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover, Monster Coming ...RAWR" >> dragon.txt
 find ~ -name dragon -type f -print
 cat dragon.txt
+# testing workflow event activities and filters
